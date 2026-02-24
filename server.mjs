da1fb02ca9049438975dcebc36b35414e859b6a1
LaboratorioInnovacion <desbloqueocelusa@gmail.com>
2026-02-24 19:25:27 -0300

    asdads

--- server.mjs
+++ server.mjs
@@ -1,11 +1,32 @@
-import http from "http";
 
-const server = http.createServer((req, res) => {
-  res.writeHead(200, { "Content-Type": "text/plain" });
-  res.end("Hello, World!\n");
-});
+// import http from "http";
 
-const PORT = 3000;
-server.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
-});
+// const server = http.createServer((req, res) => {
+//   res.writeHead(200, { "Content-Type": "text/plain" });
+//   res.end("Hello, World!\n");
+// });
+
+// const PORT = 3000;
+// server.listen(PORT, () => {
+//   console.log(`Server is running on http://localhost:${PORT}`);
+// });
+import path from 'path';
+
+// Definir una ruta de archivo de ejemplo
+const filePath = './data/example.txt';
+
+// Obtener el directorio base
+const dirName = path.dirname(filePath);
+console.log('Directorio base:', dirName);
+
+// Obtener el nombre del archivo sin extensión
+const baseName = path.basename(filePath, '.txt');
+console.log('Nombre del archivo:', baseName);
+
+// Obtener la extensión del archivo
+const extName = path.extname(filePath);
+console.log('Extensión del archivo:', extName);
+
+// Crear una ruta unida
+const newPath = path.join('/user', 'docs', 'newfile.txt');
+console.log('Nueva ruta:', newPath);
